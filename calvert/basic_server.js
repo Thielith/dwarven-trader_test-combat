8b21a4f29b917263cbd523bbc4d5eebf70a012b9
rckohler <fears_bane@yahoo.com>
2018-04-13 12:51:33 -0700

    a

--- calvert/basic_server.js
+++ calvert/basic_server.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql'); 
-var io = require('socket.io').listen(33339);
+var io = require('socket.io').listen(33333);
 const {exec} = require('child_process');
 
 var con = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "p2950",
-	database: "calvertdwarven"
+	database: "test_dwarven"
 })
 
 io.sockets.on('connection', function (socket) {
