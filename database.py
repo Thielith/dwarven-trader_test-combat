1e45494bfc26f0bcaa1e4ea7ea7dd23d4f893a48
Thielith <aydensgame@gmail.com>
2018-05-18 09:46:04 -0700

    moved combat function out of databse.py

--- database.py
+++ database.py
@@ -121,33 +121,7 @@ def getDataFromTableByID(connection, table, idName, id):
 		ret.append(v)
 #	print(ret)
 	return ret
-try:
-	if sys.argv[1] != None:
-		if sys.argv[1] == "updateUnits":
-			print("updating player" + sys.argv[8])
-			updateUnitDB(cur,sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7],sys.argv[8],sys.argv[9],sys.argv[10])
-			print("commit update")
-			db.commit();
-			print("close")
-			db.close();
-	
-		elif sys.argv[1] == "deleteStatusByUnitID":
-			print("deleting statuses with userID of " + sys.argv[2])
-			deleteStatusByUnitID(cur, "statuses", sys.argv[2])
-			print("commit update")
-			db.commit();
-			print("close")
-			db.close();
-	
-		elif sys.argv[1] == "updateStatus":
-			print("adding statuses")
-			updateStatusDB(cur, sys.argv[2], sys.argv[3], sys.argv[4])
-			print("commit update")
-			db.commit();
-			print("close")
-			db.close();
-except:
-	print("invaild call to system variable")
+
 #x = getDataFromTableByID(cur,"player","player_name","Billy")
 
 def getAllDataInTable(database, table):
